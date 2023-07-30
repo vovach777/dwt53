1a6ff76c5cd5cae20d23c5a24b5e1b6546a28605
Vladimir <vovach777@yandex.ru>
2023-07-30 22:09:50 +0300

    Update dwt53.hpp

--- dwt53.hpp
+++ dwt53.hpp
@@ -273,5 +273,90 @@ static void inv_dwt53_2d(the_matrix & data, int levels)
 
 }
 
+
+
+namespace dwt2d {
+struct Index {
+    int offs;
+    int size;
+};
+
+struct Block {
+    Index x;
+    Index y;
+};
+
+struct LevelBlock {
+    Block HL;
+    Block LH;
+    Block HH;
+};
+}
+
+static std::vector<dwt2d::LevelBlock> get_dwt_sizes(the_matrix const& a, int levels)
+{
+    auto x_sizes = calc_L_sizes(a[0].size(),levels);
+    auto y_sizes = calc_L_sizes(a.size(),levels);
+    auto min_size = std::min( x_sizes.size(), y_sizes.size() );
+    x_sizes.resize( min_size );
+    y_sizes.resize( min_size );
+    std::vector<dwt2d::LevelBlock> result(min_size);
+    //fill sizes result
+    for (int i=0; i<min_size; i++)
+    {
+        auto & lb = result[i];
+        lb.HL.x.offs = sizeof_L(x_sizes[i]); 
+        lb.HL.x.size = sizeof_H(x_sizes[i]);
+        lb.HL.y.offs = 0;
+        lb.HL.y.size = sizeof_H(y_sizes[i]);
+        lb.HH.x.offs =  lb.HL.x.offs;
+        lb.HH.y.offs =  lb.HL.y.size;
+        lb.HH.x.size = sizeof_L(y_sizes[i]); 
+        lb.HH.y.size = lb.HL.y.size;
+        lb.LH.x.offs = 0;
+        lb.LH.x.size = sizeof_L(x_sizes[i]);
+        lb.LH.y.offs = lb.HH.y.offs;
+        lb.LH.y.size = lb.HH.y.size;
+    }
+    return result;
+}
+
+static void scalar_quant_example(the_matrix & a, int levels, int Q) 
+{
+  
+    auto sizes = get_dwt_sizes(data_copy,levels);
+    for (auto const & si : sizes)
+    {
+        if (Q < 1)
+            Q = 1;
+        // std::cout << "HL:" << "x=" << si.HL.x.offs << ":" << si.HL.x.size <<  "  y=" << si.HL.y.offs << ":" << si.HL.y.size << std::endl;
+        // std::cout << "LH:" << "x=" << si.LH.x.offs << ":" << si.LH.x.size <<  "  y=" << si.LH.y.offs << ":" << si.LH.y.size << std::endl;
+        // std::cout << "HH:" << "x=" << si.HH.x.offs << ":" << si.HH.x.size <<  "  y=" << si.HH.y.offs << ":" << si.HH.y.size << std::endl;
+        // std::cout << "----" << std::endl;
+        for (int y=0; y < si.HH.y.size; ++y)
+        for (int x=0; x < si.HH.x.size; ++x)
+        {
+            auto & coof_hh = data_copy[x + si.HH.x.offs][y + si.HH.y.offs];
+            
+            coof_hh = ((coof_hh + (Q)) / (Q*2)) * (Q*2);
+        }
+        for (int y=0; y < si.HL.y.size; ++y)
+        for (int x=0; x < si.HL.x.size; ++x)
+        {
+            auto & coof_hl = data_copy[x + si.HL.x.offs][y + si.HL.y.offs];
+            
+            coof_hl = ((coof_hl + (Q/2)) / Q) * Q;
+        }
+        for (int y=0; y < si.LH.y.size; ++y)
+        for (int x=0; x < si.LH.x.size; ++x)
+        {
+            auto & coof_lh = data_copy[x + si.LH.x.offs][y + si.LH.y.offs];
+            
+            coof_lh = ((coof_lh + (Q/2)) / Q) * Q;
+        }
+        Q /= 2;
+  
+    }
+}
 #endif
 #endif

70acc7b93feb882fb657e1a004c7f7f8dae77a1d
Vladimir <vovach777@yandex.ru>
2023-08-14 11:25:05 +0300

    Delete huffmanpack.hpp

--- huffmanpack.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include "the_matrix.hpp"
-namespace huffman {
-std::vector<uint8_t> compress(const the_matrix& matrix);
-the_matrix decompress(const std::vector<uint8_t>& compressed);
-};
